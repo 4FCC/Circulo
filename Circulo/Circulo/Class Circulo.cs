aac214fb90ebe2be83574712b8a081cab1737de7
AFCC <113411810+GrymmKFC@users.noreply.github.com>
2023-03-28 22:10:39 -0600

    Add files via upload

---
+++ Circulo/Circulo/Class Circulo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circulo
+{
+
+    public class _CRadio
+    {
+
+        public double pi = Math.PI;
+        public double _Radio { get; set;} 
+        
+        public _CRadio(double _Radio)
+        {
+
+            this._Radio = _Radio;
+
+        }
+
+        public void Perimetro()
+        {
+
+            Console.WriteLine("Su Perimetro es " + (2 * _Radio * pi));
+
+        }
+
+        public void Area()
+        {
+
+            double potencia = Math.Pow(_Radio, 2);
+            Console.WriteLine("Su area es " + (pi * potencia));
+
+        }
+
+        public void Volumen()
+        {
+
+            double potencia = Math.Pow(_Radio, 3);
+            Console.WriteLine("Su volumen es " + ((4 * pi * potencia) / (3)));
+
+        }
+
+    }
+
+}
+
---
+++ Circulo/Circulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Circulo;
+
+
+// Circulo
+
+Console.WriteLine("Ingresa el readio de tu circulo");
+int number = Int32.Parse(Console.ReadLine());
+_CRadio o_CRadio = new _CRadio(number);
+
+o_CRadio.Perimetro();
+o_CRadio.Area();
+o_CRadio.Volumen();
+
+Console.ReadKey();
